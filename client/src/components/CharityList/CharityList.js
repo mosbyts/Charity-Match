0e5784b1b913e1961d5f71b1fb03b0b1b49e8b8d
Tianna Mosby <mosbyts@vcu.edu>
2020-01-11 10:38:42 -0500

    Updated some components.

--- client/src/components/CharityList/CharityList.js
+++ client/src/components/CharityList/CharityList.js
@@ -1,11 +1,16 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 
-// export default class CharityList extends Component {
-//   render() {
-//     if (!this.state.charities) return <div>Loading..</div>;
-//     const items = this.state.charities.map(charity =>
-//       <li key={charity}>{charity}</li>
-//     );
-//     return <ul>{items}</ul>;
-//   }
-// });
+export default class CharityList extends Component {
+  render() {
+    return(
+        <div>
+            
+        </div>
+    )
+    // if (!this.state.charities) return <div>Loading..</div>;
+    // const items = this.state.charities.map(charity =>
+    //   <li key={charity}>{charity}</li>
+    // );
+    // return <ul>{items}</ul>;
+  }
+};
--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -7,6 +7,29 @@ class Footer extends Component {
       return (
           <div>
               <footer>
+                <Grid>
+                    <Grid.Column mobile={16} tablet={8} computer={4}>
+                        <h2>Learn More</h2>
+                            <ul>
+                                <li>About Us</li>
+                                <li>Contact Us</li>
+                            </ul>
+                    </Grid.Column>
+                    <Grid.Column mobile={16} tablet={8} computer={4}>
+                        <h2>Contribute</h2>
+                            <ul>
+                                <li>Log In</li>
+                                <li>Sign Up</li>
+                            </ul>
+                    </Grid.Column>
+                    <Grid.Column mobile={16} tablet={8} computer={4}>
+                        <h2>Legal</h2>
+                            <ul>
+                                <li>Privacy</li>
+                                <li>Terms and Conditions</li>
+                            </ul>
+                    </Grid.Column>
+                </Grid>
                   <Grid verticalAlign='middle'>
                       <Grid.Row textAlign='center' columns={1}>
                           <GridColumn>
