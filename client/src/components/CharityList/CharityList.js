80a9817b2dacc392a1a16821ff2333dab05a38e4
Tianna Mosby <mosbyts@vcu.edu>
2020-01-11 13:12:28 -0500

    Made initial layout for charity list.

--- client/src/components/CharityList/CharityList.js
+++ client/src/components/CharityList/CharityList.js
@@ -8,14 +8,20 @@ export default class CharityList extends Component {
         <div>
           <Grid>
             <Grid.Row>
-              <Grid.Column width={3}>
+              <Grid.Column width={1}></Grid.Column>
+              <Grid.Column width={2}>
                 <Image src={ Logo } size='small'/>
               </Grid.Column>
-              <Grid.Column width={8}>
+              <Grid.Column width={7}>
                 <h5>Charity Name</h5>
                 <p>Charity Mission</p>
                 <p>Charity Info: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
               </Grid.Column>
+              <Grid.Column width={1}></Grid.Column>
+              <Grid.Column width={4}>
+                <Image src="https://prodevsblog.com/uploads/blog_image/news_24_1.png" size='big'></Image>
+              </Grid.Column>
+              <Grid.Column width={1}></Grid.Column>
             </Grid.Row>
           </Grid>
         </div>
