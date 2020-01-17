ed66c48edd4339afc75c4570dccee4e459a4cb28
Tayvon Pierre Randall <PierreMac@VCUPierre.local>
2020-01-16 21:17:21 -0500

    fixed survey

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -4,6 +4,7 @@ import { Grid, GridColumn } from 'semantic-ui-react';
 import './style.css';
 import About from "../../About/About";
 import Contact from "../../Contact/Contact";
+import Survey from "../../Survey/Survey";
 
 class Footer extends Component {
     render() {
@@ -17,6 +18,7 @@ class Footer extends Component {
                             <ul>
                                 <Link to="/about" component={About}><li>About Us</li></Link>
                                 <Link to="/contact" component={Contact}><li>Contact Us</li></Link>
+                                <Link to="/survey" component={Survey}><li>Survey</li></Link>
                             </ul>
                     </Grid.Column>
                     <Grid.Column mobile={16} tablet={8} computer={4}>
