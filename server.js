77211bdafd73da75de375a8d20774c5b8642497c
Tianna Mosby <mosbyts@vcu.edu>
2020-01-30 09:23:18 -0500

    Updated Heroku info.

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-// const mysql = require('mysql');
 // var db = require("./models/index.js");
-// const multer = require("multer");
 const cors = require("cors");
 const router = require("./routes/api");
 
@@ -29,27 +27,6 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 // Passport middleware and config------------------------------------------------
 
-// //Allows user to upload images using Multer & CORS-------------------------------
-// var storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, 'public/images/uploads')
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + "-" + file.originalname)
-//   }
-// });
-
-// const upload = multer({ storage })
-
-// app.post('/upload', upload.single('image'), (req, res) =>{
-//   if(req.file)
-//     res.json({
-//      imageUrl: `images/uploads/${req.file.filename}`
-//     });
-//    else
-//      res.status("409").json("No Files to Upload."); 
-// });
-
 // Serve Static Build file in Production
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
@@ -59,19 +36,15 @@ if(process.env.NODE_ENV === 'production') {
   });
 }
 
-// app.use(express.static(path.join(__dirname,"./client/build")));
+app.use(express.static(path.join(__dirname,"./client/build")));
 
-// // Routes------------------------------------------------------------------------
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get('/secure', authenticationRequired, (req, res) => {
+  res.json(req.jwt);
+});
 
-// app.get('/secure', authenticationRequired, (req, res) => {
-//   res.json(req.jwt);
-// });
 const db = require("./config/keys").mongoURI;
 // Connect to the Mongo DB
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Charity_Match");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Charity_Match");
 mongoose.connect(
   db, {useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false } 
 ).then(()=> console.log("connected to mongodb"))
@@ -83,31 +56,3 @@ app.use("/api", router);
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
-
-
-
-//Allows user to upload images using Multer & CORS-------------------------------
-
-// //Connects to MySQL database and JawsDB on Heroku--------------------------------
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user:'root',
-//   password:'password',
-//   database: 'userData'
-//   });
-  
-//   connection.connect(function(err){
-//     (err)? console.log(err): console.log('Connected.');
-//   });
-
-//   if (process.env.JAWSDB_URL === "production") {
-//     app.use(express.static("client/build"));
-//   }
-// //Connects to MySQL database and JawsDB on Heroku--------------------------------
-// // Routes------------------------------------------------------------------------
-
-// db.sequelize.sync({ force: true }).then(function(){
-//     app.listen(PORT, function() {
-//       console.log(`🌎 ==> API server now on port ${PORT}!`);
-//     });
-//   });
